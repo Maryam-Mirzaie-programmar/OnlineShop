1385334059dd87b7d5e85783c132a7541e3001f7
Maryam <maryam.mirzaie.programmar@gmail.com>
2022-10-24 14:24:29 +0330

    Register - making RegisterViewModel and Register View

---
+++ Datalayer/ViewModels/AccountViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datalayer
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(250)]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد.")]
+        public string Email { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار رمز عبور")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(100)]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "لطفا رمز عبور را در این فیلد تکرار نمایید")]
+        public string RePassword { get; set; }
+
+
+    }
+}
