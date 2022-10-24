e9db98dc16402699b3992afc6244cc95e7d7440b
Maryam <maryam.mirzaie.programmar@gmail.com>
2022-10-24 12:56:29 +0330

    Adding MetaData to Model (RoleMetaData & UserMetaData)

---
+++ Datalayer/MetaDataClasses/RoleMetaData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datalayer
+{
+    public class RoleMetaData
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name = "عنوان نقش")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(150)]
+        public string RoleTitle { get; set; }
+
+        [Display(Name = "عنوان سیستمی نقش")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(150)]
+        public string RoleName { get; set; }
+    }
+
+
+    [MetadataType(typeof(RoleMetaData))]
+    public partial class Role
+    {
+
+    }
+}
---
+++ Datalayer/MetaDataClasses/UserMetaData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datalayer
+{
+    public class UserMetaData
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "نقش")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        public int RoleId { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(250)]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد.")]
+        public string Email { get; set; }
+
+        [Display(Name = "کد فعالسازی")]
+        [Required(ErrorMessage = "لطفا فیلد {0} را وارد نمایید")]
+        [MaxLength(50)]
+        public string ActrivationCode { get; set; }
+
+        [Display(Name = "فعال")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "تاریخ ثبتنام")]
+        public System.DateTime RegisterDate { get; set; }
+    }
+
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+
+    }
+}
