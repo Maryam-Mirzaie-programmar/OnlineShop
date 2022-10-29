feae4ee98fd2e1f4c4f3f008c486dddac3e25255
Maryam <maryam.mirzaie.programmar@gmail.com>
2022-10-29 09:49:25 +0330

    Show categories in sidebar _DefaulyLayout.cshtml

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Datalayer;
+
+namespace MyEshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        OnlineShopDBEntities db = new OnlineShopDBEntities();
+
+
+        [ChildActionOnly]
+        public ActionResult ShowProductGroups()
+        {
+            return PartialView(db.Product_Groups);   
+        }
+    }
+}
