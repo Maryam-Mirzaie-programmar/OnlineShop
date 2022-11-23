d21111e63c9fad42a84fddca8d65f766c66b0e9f
Maryam <maryam.mirzaie.programmar@gmail.com>
2022-11-23 10:07:45 +0330

    Search feature completed

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Datalayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEshop.Controllers
+{
+    public class SearchController : Controller
+    {
+        OnlineShopDBEntities db = new OnlineShopDBEntities();
+
+        // GET: Search
+        public ActionResult Index(string q)
+        {
+            var products = new List<Product>();
+            if(! String.IsNullOrWhiteSpace(q))
+            {
+                q = q.ToLower().Trim();
+                products.AddRange(db.Products.Where(p => p.ProductTitle.ToLower().Contains(q)));
+                products.AddRange(db.Products.Where(p => p.ProductShortDescription.ToLower().Contains(q)));
+                products.AddRange(db.Product_Tags.Where(t => t.ProductTag == q).Select(t => t.Product));
+            }
+            return View(products.Distinct());
+        }
+    }
+}
