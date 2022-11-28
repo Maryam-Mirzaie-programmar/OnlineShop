0e8ce40286dcc41aa0438303e0fe6877cfe72dc0
Maryam <maryam.mirzaie.programmar@gmail.com>
2022-11-28 14:02:27 +0330

    Basket-Side & Basket-Count

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,73 @@
+﻿using Datalayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEshop.Controllers
+{
+    public class ShopController : Controller
+    {
+        OnlineShopDBEntities db = new OnlineShopDBEntities();
+        // GET: Shop
+        public int ShopItemsCount()
+        {
+            List<ShopCartItem> cartItems = new List<ShopCartItem>();
+            if (Session["ShopCart"] != null)
+            {
+                cartItems = Session["ShopCart"] as List<ShopCartItem>;
+            }
+            return cartItems.Sum(p => p.Count);
+        }
+
+        public int AddToCart(int? id)
+        {
+            List<ShopCartItem> cartItems = new List<ShopCartItem>();
+            if (Session["ShopCart"] != null)
+            {
+                cartItems = Session["ShopCart"] as List<ShopCartItem>;
+            }
+            if (id != null && db.Products.Any(p => p.ProductID == id))
+            {
+                if (cartItems.Any(p => p.ProductID == id))
+                {
+                    int index = cartItems.FindIndex(p => p.ProductID == id);
+                    cartItems[index].Count += 1;
+                }
+                else
+                {
+                    cartItems.Add(new ShopCartItem()
+                    {
+                        ProductID = id.Value,
+                        Count = 1
+                    });
+                }
+            }
+            Session["ShopCart"] = cartItems;
+            return ShopItemsCount();
+        }
+
+        public ActionResult ShowBasketSide()
+        {
+            List<ShopCartItemViewModel> cartItems = new List<ShopCartItemViewModel>();
+            if (Session["ShopCart"] != null)
+            {
+               List<ShopCartItem> itemsList = Session["ShopCart"] as List<ShopCartItem>;
+                foreach(var item in itemsList)
+                {
+                    var product = db.Products.Where(p => p.ProductID == item.ProductID).Select(p => new { p.ProductImageName, p.ProductTitle}).Single();
+                    cartItems.Add(new ShopCartItemViewModel()
+                    {
+                        ProductID =item.ProductID,
+                        Count = item.Count,
+                        Title = product.ProductTitle,
+                        ImageName = product.ProductImageName
+
+                    });
+                }
+            }
+            return PartialView(cartItems);
+        }
+    }
+}
---
+++ Datalayer/ViewModels/ShopCartViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalayer
+{
+    public class ShopCartItem
+    {
+        public int ProductID { get; set; }
+        public int Count { get; set; }
+    }
+    public class ShopCartItemViewModel
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+        public int Count { get; set; }
+    }
+}
